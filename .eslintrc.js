4430889f5f9e8b559ebbf52c6ff12e2c160838b1
Lucas Mazza <lucas.mazza@plataformatec.com.br>
2016-07-22 12:15:27 -0300

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'globals': {
+    '$': true
+  },
+  'ecmaFeatures': {
+    'modules': true
+  },
+  'rules': {
+    'camelcase': 2,
+    'comma-dangle': [2, 'never'],
+    'comma-style': [2, 'last'],
+    'eqeqeq': 2,
+    'indent': [2, 2, { 'VariableDeclarator': 2 }],
+    'no-eq-null': 2,
+    'no-extra-parens': 2,
+    'no-extra-semi': 2,
+    'no-lonely-if': 2,
+    'no-multi-spaces': 0,
+    'no-nested-ternary': 2,
+    'no-param-reassign': 2,
+    'no-self-compare': 2,
+    'no-shadow': 2,
+    'no-throw-literal': 2,
+    'no-undef': 2,
+    'no-underscore-dangle': 0,
+    'no-void': 2,
+    'quotes': [2, 'single'],
+    'semi': [2, 'always']
+  }
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+# Plataformatec linters and static analysis configuration files
+
+A collection of configuration files we use for source code linters and static
+analysis tools.
+
+* [ESLint](https://github.com/eslint/eslint) - [`.eslintrc.js`](.eslintrc.js)
+* [Reek](http://github.com/troessner/reek) - [`.defaults.reek`](.defaults.reek)
+* [RuboCop](http://github.com/bbatsov/rubocop/pull/2416) - [`.rubocop.yml`](.rubocop.yml)
+* [SCSS Lint](https://github.com/brigade/scss-lint) - [`.scss-lint.yml`](.scss-lint.yml)
